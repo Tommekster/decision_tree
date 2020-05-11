7456e52b6d09f76cf4795e64543449c78ca56dc1
Tomas Zikmund <tommekster@gmail.com>
2020-05-11 10:58:29 +0200

    get_target_distribution

--- decision_tree/__init__.py
+++ decision_tree/__init__.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from typing import List, Tuple
+from typing import List, Tuple, Callable, Dict, Hashable
 
 
 def parse_table(path: str, delimiter: str = ",") -> List[Tuple[str]]:
@@ -12,7 +12,26 @@ def parse_table(path: str, delimiter: str = ",") -> List[Tuple[str]]:
     return list(read_file())
 
 
+def get_target_distribution(table: List[Tuple[str]]):
+    groups = group_by(table, lambda x: x[-1])
+    total_rows = len(table)
+    return [
+        (key, len(vals), len(vals) / total_rows)
+        for key, vals in groups.items()
+    ]
+
+
+def group_by(table: List[Tuple[str]], selector: Callable[[Tuple[str]], Hashable]) -> Dict[Hashable, List[Tuple[str]]]:
+    groups = dict()
+    for row in table:
+        target = selector(row)
+        groups.setdefault(target, []).append(row)
+    return groups
+
+
 if __name__ == "__main__":
-    for l in parse_table("../data/cars/car.data"):
-        print("\t".join(l))
+    table = parse_table("../data/cars/car.data")
+    print("\t".join(["Target", "Cnt", "%"]))
+    for target, cnt, frac in get_target_distribution(table):
+        print("\t".join([target, str(cnt), str(frac * 100)]))
     pass
