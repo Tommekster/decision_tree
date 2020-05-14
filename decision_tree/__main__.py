70d6f4bdb2ffceb2f016e59218b53d45e2779ff4
Tomas Zikmund <tommekster@gmail.com>
2020-05-14 23:15:09 +0200

    parametrized renyi entropy in decision-tree generator

--- decision_tree/__main__.py
+++ decision_tree/__main__.py
@@ -2,14 +2,11 @@
 
 from typing import List, Tuple, Union
 
-import blockdiag.command
-import graphviz
-
+from . import diagram
 from . import entropy
 from . import table_repository
 from .decision_tree_generator import DecisionTreeGenerator
 from .group_by import group_by
-from .models import DecisionNode, Leaf
 
 
 def get_target_distribution(table: List[Tuple[str]]):
@@ -35,56 +32,30 @@ def load_cars() -> Tuple[List[Tuple[str]], Tuple[Union[int, str], ...]]:
     )
 
 
-def export_graph(tree: Union[DecisionNode, List[Leaf]]) -> str:
-    graph = graphviz.Digraph()
-    index = [0]
-
-    def get_index():
-        index[0] += 1
-        return index[0]
-
-    def add_node(node, node_index):
-        if isinstance(node, DecisionNode):
-            graph.node(str(node_index), node.label)
-            for branch in node.branches:
-                if isinstance(branch.children, DecisionNode):
-                    child_index = get_index()
-                    add_node(branch.children, child_index)
-                    graph.edge(str(node_index), str(child_index), label=branch.value)
-                elif isinstance(branch.children, list):
-                    child_index = get_index()
-                    graph.node(
-                        str(child_index),
-                        "\\n".join(
-                            "{} ({})".format(child.value, child.count)
-                            for child in branch.children
-                        )
-                    )
-                    graph.edge(str(node_index), str(child_index), label=branch.value)
-                else:
-                    raise NotImplementedError
-        else:
-            raise NotImplementedError
-
-    add_node(tree, get_index())
-
-    return graph.source
-
-
 if __name__ == "__main__":
-    generator = DecisionTreeGenerator(entropy.shannon_entropy)
     table, labels = load_golf()
     print("\t".join(["Target", "Cnt", "%"]))
     for target, cnt, frac in get_target_distribution(table):
         print("\t".join([target, str(cnt), str(frac * 100)]))
-    print("Total entropy", entropy.shannon_entropy(table))
-    print("Total entropy for target", entropy.shannon_entropy([x[-1] for x in table]))
-    print("max gain {} has index {}".format(*generator.select_feature_index(table)))
-    tree = generator.create_tree(table, labels=labels)
 
-    with open("output.json", "w") as f:
-        f.write(tree.to_json() + "\n")
-    with open("diag.diag", "w") as f:
-        f.write(export_graph(tree).replace("digraph", "blockdiag"))
-    blockdiag.command.main(["diag.diag"])
-    # print(tree.to_json(indent=2))
+    json_results = False
+    diagram_results = True
+
+    alphas = [0.0, 0.5, 1.0, 2.0, float("inf")]
+    results = {}
+    for alpha in alphas:
+        generator = DecisionTreeGenerator(lambda x: entropy.renyi_entropy(x, alpha))
+        print("Total entropy", generator.entropy(table))
+        print("Total entropy for target", generator.entropy([x[-1] for x in table]))
+        print("max gain {} has index {}".format(*generator.select_feature_index(table)))
+
+        tree = generator.create_tree(table, labels=labels)
+        results[alpha] = tree.to_json()
+        if json_results:
+            with open("output_{}.json".format(alpha), "w") as f:
+                f.write(results[alpha] + "\n")
+
+        if diagram_results:
+            diagram_file = "diagram{}.png".format(alpha)
+            graph = diagram.create_graph(tree)
+            diagram.save_graph(graph, diagram_file)
